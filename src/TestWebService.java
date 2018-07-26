ae52f4eebc812d82ac6c10d707c4ffc10abb2cb7
Avinash <Avinash@Avinash-PC>
2018-07-26 23:12:36 +0530

    test commit
    Change-Id: Ifcf54a59ef4626cc6e97518b9ba365f916f3b92e
    Signed-off-by: Avinash <Avinash@Avinash-PC>

---
+++ src/TestWebService.java
@@ -0,0 +1,16 @@
+
+public class TestWebService {
+
+	public int addNumbers(int a, int b) {
+		
+		return a + b;
+	}
+	
+	public int multiplyNumbers(int a, int b) {
+		return a*b;
+	}
+	
+	public int divideNum(int numerator, int denominator) {
+		return numerator/denominator;
+	}
+}
